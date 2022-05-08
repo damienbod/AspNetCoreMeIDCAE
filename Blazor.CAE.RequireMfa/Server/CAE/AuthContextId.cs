721897268c970b74b694d6bd88a7a3e18809b47d
damienbod <damien_bod@hotmail.com>
2022-05-08 11:18:59 +0200

    Add Blazor project

---
+++ Blazor.CAE.RequireMfa/Server/CAE/AuthContextId.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.CAE.RequireMfa.Server;
+
+public static class AuthContextId
+{
+    public const string C1 = "c1";
+    public const string C2 = "c2";
+    public const string C3 = "c3";
+}
