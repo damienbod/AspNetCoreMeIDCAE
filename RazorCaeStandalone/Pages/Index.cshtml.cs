e3575cea7a66412013bd893aca0d0351513da178
damienbod <damien_bod@hotmail.com>
2022-05-15 15:38:55 +0200

    init razor app

---
+++ RazorCaeStandalone/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorCaeStandalone.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
