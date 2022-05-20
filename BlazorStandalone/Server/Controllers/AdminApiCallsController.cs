c0e28ebf02312cd08440b767add35c7225f54109
damienbod <damien_bod@hotmail.com>
2022-05-20 15:57:29 +0200

    code clean up

--- BlazorStandalone/Server/Controllers/AdminApiCallsController.cs
+++ BlazorStandalone/Server/Controllers/AdminApiCallsController.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Web;
 
 namespace BlazorBffAzureAD.Server.Controllers;
 
