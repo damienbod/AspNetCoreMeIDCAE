147b819433527ae24efdb8d254ad8c80fa762238
damienbod <damien_bod@hotmail.com>
2022-05-22 07:05:10 +0200

    code clean up

--- BlazorStandalone/Server/Startup.cs
+++ BlazorStandalone/Server/Startup.cs
@@ -1,95 +1,95 @@
-using BlazorBffAzureAD.Server.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
+//using BlazorBffAzureAD.Server.Services;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Identity.Web;
+//using Microsoft.Identity.Web.UI;
 
-namespace BlazorBffAzureAD.Server;
+//namespace BlazorBffAzureAD.Server;
 
-public class Startup
-{
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+//public class Startup
+//{
+//    public Startup(IConfiguration configuration)
+//    {
+//        Configuration = configuration;
+//    }
 
-    public IConfiguration Configuration { get; }
+//    public IConfiguration Configuration { get; }
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddScoped<MsGraphService>();
-        services.AddScoped<CaeClaimsChallengeService>();
+//    public void ConfigureServices(IServiceCollection services)
+//    {
+//        services.AddScoped<MsGraphService>();
+//        services.AddScoped<CaeClaimsChallengeService>();
 
-        services.AddAntiforgery(options =>
-        {
-            options.HeaderName = "X-XSRF-TOKEN";
-            options.Cookie.Name = "__Host-X-XSRF-TOKEN";
-            options.Cookie.SameSite = SameSiteMode.Strict;
-            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-        });
+//        services.AddAntiforgery(options =>
+//        {
+//            options.HeaderName = "X-XSRF-TOKEN";
+//            options.Cookie.Name = "__Host-X-XSRF-TOKEN";
+//            options.Cookie.SameSite = SameSiteMode.Strict;
+//            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+//        });
 
-        services.AddHttpClient();
-        services.AddOptions();
+//        services.AddHttpClient();
+//        services.AddOptions();
 
-        var scopes = Configuration.GetValue<string>("DownstreamApi:Scopes");
-        string[] initialScopes = scopes.Split(' ');
+//        var scopes = Configuration.GetValue<string>("DownstreamApi:Scopes");
+//        string[] initialScopes = scopes.Split(' ');
 
-        services.AddMicrosoftIdentityWebAppAuthentication(Configuration)
-            .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
-            .AddMicrosoftGraph("https://graph.microsoft.com/v1.0", scopes)
-            .AddInMemoryTokenCaches();
+//        services.AddMicrosoftIdentityWebAppAuthentication(Configuration)
+//            .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+//            .AddMicrosoftGraph("https://graph.microsoft.com/v1.0", scopes)
+//            .AddInMemoryTokenCaches();
 
-        services.AddControllersWithViews(options =>
-            options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+//        services.AddControllersWithViews(options =>
+//            options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
 
-        services.AddRazorPages().AddMvcOptions(options =>
-        {
-            //var policy = new AuthorizationPolicyBuilder()
-            //    .RequireAuthenticatedUser()
-            //    .Build();
-            //options.Filters.Add(new AuthorizeFilter(policy));
-        }).AddMicrosoftIdentityUI();
-    }
+//        services.AddRazorPages().AddMvcOptions(options =>
+//        {
+//            //var policy = new AuthorizationPolicyBuilder()
+//            //    .RequireAuthenticatedUser()
+//            //    .Build();
+//            //options.Filters.Add(new AuthorizeFilter(policy));
+//        }).AddMicrosoftIdentityUI();
+//    }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseWebAssemblyDebugging();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Error");
-        }
+//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//    {
+//        if (env.IsDevelopment())
+//        {
+//            app.UseDeveloperExceptionPage();
+//            app.UseWebAssemblyDebugging();
+//        }
+//        else
+//        {
+//            app.UseExceptionHandler("/Error");
+//        }
 
-        app.UseSecurityHeaders(
-            SecurityHeadersDefinitions.GetHeaderPolicyCollection(env.IsDevelopment(),
-                Configuration["AzureAd:Instance"]));
+//        app.UseSecurityHeaders(
+//            SecurityHeadersDefinitions.GetHeaderPolicyCollection(env.IsDevelopment(),
+//                Configuration["AzureAd:Instance"]));
 
-        app.UseHttpsRedirection();
-        app.UseBlazorFrameworkFiles();
-        app.UseStaticFiles();
+//        app.UseHttpsRedirection();
+//        app.UseBlazorFrameworkFiles();
+//        app.UseStaticFiles();
 
-        app.UseRouting();
+//        app.UseRouting();
 
-        app.UseNoUnauthorizedRedirect("/api");
+//        app.UseNoUnauthorizedRedirect("/api");
 
-        app.UseAuthentication();
-        app.UseAuthorization();
+//        app.UseAuthentication();
+//        app.UseAuthorization();
 
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapRazorPages();
-            endpoints.MapControllers();
-            endpoints.MapNotFound("/api/{**segment}");
-            endpoints.MapFallbackToPage("/_Host");
-        });
+//        app.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapRazorPages();
+//            endpoints.MapControllers();
+//            endpoints.MapNotFound("/api/{**segment}");
+//            endpoints.MapFallbackToPage("/_Host");
+//        });
 
-    }
-}
+//    }
+//}
