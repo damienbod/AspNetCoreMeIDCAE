1ec45b5a0bf58d272e3d2d761149d4bf3ad4af94
damienbod <damien_bod@hotmail.com>
2022-10-07 19:07:38 +0200

    init solution

---
+++ RazorCaePhishingResistant/Cae/AuthContextId.cs
@@ -0,0 +1,8 @@
+﻿namespace RazorCaeStandalone;
+
+public static class AuthContextId
+{
+    public const string C1 = "c1";
+    public const string C2 = "c2";
+    public const string C3 = "c3";
+}
---
+++ RazorCaePhishingResistant/Pages/Admin.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace RazorCaeStandalone.Pages
+{
+    public class AdminModel : PageModel
+    {
+        private readonly CaeClaimsChallengeService _caeClaimsChallengeService;
+
+        public AdminModel( CaeClaimsChallengeService caeClaimsChallengeService)
+        {
+            _caeClaimsChallengeService = caeClaimsChallengeService;
+        }
+
+        [BindProperty]
+        public IEnumerable<string>? Data { get; private set; }
+
+        public IActionResult OnGet()
+        {
+            // if CAE claim missing in id token, the required claims challenge is returned
+            var claimsChallenge = _caeClaimsChallengeService
+                .CheckForRequiredAuthContextIdToken(AuthContextId.C1, HttpContext);
+
+            if (claimsChallenge != null)
+            {
+                var properties = new AuthenticationProperties { RedirectUri = "/admin" };
+
+                properties.Items["claims"] = claimsChallenge;
+                return Challenge(properties);
+            }
+
+            Data = new List<string>()
+            {
+                "Admin data 1",
+                "Admin data 2"
+            };
+
+            return Page();
+        }
+    }
+}
