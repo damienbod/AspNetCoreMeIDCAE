bd16ed2fc1f462f7a08d5392cfb8ada77dee1a3e
damienbod <damien_bod@hotmail.com>
2022-10-08 16:12:20 +0200

    Adding new contexts

--- CaeAdministrationTool/CAE/AuthContextId.cs
+++ CaeAdministrationTool/CAE/AuthContextId.cs
@@ -5,4 +5,7 @@ public static class AuthContextId
     public const string C1 = "c1";
     public const string C2 = "c2";
     public const string C3 = "c3";
+    // saved for the UI created auth context
+    //public const string C4 = "c4";
+    public const string C5 = "c5";
 }
